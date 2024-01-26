3723a3aebc456edc335e98ae156d9cd8d62db672
yesoer <luisenglaender@web.de>
2024-01-26 22:38:47 +0100

    feat(main): sample usage of the widget
    Will later become the application installable by go install

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Fyne NeoVim Example")
+	w.Resize(fyne.NewSize(800, 600))
+
+	nvim := New()
+	nvim.Resize(fyne.NewSize(800, 600))
+	nvim.SetText("Hello, World!")
+	w.SetContent(nvim)
+
+	fmt.Println("show and run")
+	w.ShowAndRun()
+}
